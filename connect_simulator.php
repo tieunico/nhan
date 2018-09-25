a97bdfe32a3966419cc6dfdf2ba64ccf9d2045ca
tieunico <43010384+tieunico@users.noreply.github.com>
2018-09-25 16:04:22 -0500

    Update connect_simulator.php

--- connect_simulator.php
+++ connect_simulator.php
@@ -1,4 +1,14 @@
 <?php
+/*
+If you want to connect to the simulator
+First: enable socket inside the php.ini file
+How to enable socket is shown below.
+The php.ini is in this directory xampp>>php>>php.ini
+Open php.ini file with notepad or notepadd++
+Then search for ;extension=sockets
+Change ;extension=sockets to extension=sockets and save it.
+Now you can run the code.
+*/
 $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 
     $msg = "bay_1:open";
