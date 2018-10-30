ebdeb0268486088a5d0ff127137f2542efd72beb
tieunico <43010384+tieunico@users.noreply.github.com>
2018-10-29 19:25:09 -0500

    new query format

--- new/mydata.php
+++ new/mydata.php
@@ -20,7 +20,7 @@ if(mysqli_connect_error()){
         FROM breaker1
         INNER JOIN breaker2
         WHERE breaker1.ID=breaker2.ID;";*/
-$sql="SELECT * FROM breaker1 WHERE ID=(SELECT max(ID) FROM breaker1)
+/*$sql="SELECT * FROM breaker1 WHERE ID=(SELECT max(ID) FROM breaker1)
 UNION ALL
 SELECT * FROM breaker2 WHERE ID=(SELECT max(ID) FROM breaker2)
 UNION ALL
@@ -43,12 +43,90 @@ UNION ALL
 SELECT * FROM breaker_aux WHERE ID=(SELECT MAX(ID) FROM breaker_aux)
 UNION ALL
 SELECT * FROM breaker_tie WHERE ID=(SELECT MAX(ID) FROM breaker_tie)";
-$result=mysqli_query($conn, $sql);
+*/
+$sql1  =  "SELECT * FROM bay_1   order by id DESC LIMIT 1";
+$sql2  =  "SELECT * FROM bay_2   order by id DESC LIMIT 1";
+$sql3  =  "SELECT * FROM bay_3   order by id DESC LIMIT 1";
+$sql4  =  "SELECT * FROM bay_4   order by id DESC LIMIT 1";
+$sql5  =  "SELECT * FROM bay_5   order by id DESC LIMIT 1";
+$sql6  =  "SELECT * FROM bay_6   order by id DESC LIMIT 1";
+$sql7  =  "SELECT * FROM bay_7   order by id DESC LIMIT 1";
+$sql8  =  "SELECT * FROM bay_8_9 order by id DESC LIMIT 1";
+$sql9  =  "SELECT * FROM m1      order by id DESC LIMIT 1";
+$sql10 =  "SELECT * FROM m2      order by id DESC LIMIT 1";
+$sql11 =  "SELECT * FROM aux     order by id DESC LIMIT 1";
+$sql12 =  "SELECT * FROM tie     order by id DESC LIMIT 1";
 
-$arrayName = array( );
-while ($row=mysqli_fetch_assoc($result)) {
-    $arrayName[]=$row;
+$result1=mysqli_query($conn, $sql1);
+$result2=mysqli_query($conn, $sql2);
+$result3=mysqli_query($conn, $sql3);
+$result4=mysqli_query($conn, $sql4);
+$result5=mysqli_query($conn, $sql5);
+$result6=mysqli_query($conn, $sql6);
+$result7=mysqli_query($conn, $sql7);
+$result8=mysqli_query($conn, $sql8);
+$result9=mysqli_query($conn, $sql9);
+$result10=mysqli_query($conn, $sql10);
+$result11=mysqli_query($conn, $sql11);
+$result12=mysqli_query($conn, $sql12);
+
+$arrayName1 = array();
+while ($row=mysqli_fetch_assoc($result1)){
+    $arrayName1[]=$row;
 }
+$arrayName2 = array();
+while ($row=mysqli_fetch_assoc($result2)){
+    $arrayName2[]=$row;
+}
+$arrayName3 = array();
+while ($row=mysqli_fetch_assoc($result3)){
+    $arrayName3[]=$row;
+}
+$arrayName4 = array();
+while ($row=mysqli_fetch_assoc($result4)){
+    $arrayName4[]=$row;
+}
+$arrayName5 = array();
+while ($row=mysqli_fetch_assoc($result5)){
+    $arrayName5[]=$row;
+}
+$arrayName6 = array();
+while ($row=mysqli_fetch_assoc($result6)){
+    $arrayName6[]=$row;
+}
+$arrayName7 = array();
+while ($row=mysqli_fetch_assoc($result7)){
+    $arrayName7[]=$row;
+}
+$arrayName8 = array();
+while ($row=mysqli_fetch_assoc($result8)){
+    $arrayName8[]=$row;
+}
+$arrayName9 = array();
+while ($row=mysqli_fetch_assoc($result9)){
+    $arrayName9[]=$row;
+}
+$arrayName10 = array();
+while ($row=mysqli_fetch_assoc($result10)){
+    $arrayName10[]=$row;
+}
+$arrayName11 = array();
+while ($row=mysqli_fetch_assoc($result11)){
+    $arrayName11[]=$row;
+}
+$arrayName12 = array();
+while ($row=mysqli_fetch_assoc($result12)){
+    $arrayName12[]=$row;
+}
+
+$final_array=array_merge(   $arrayName1,$arrayName2,
+                            $arrayName3,$arrayName4,
+                            $arrayName5,$arrayName6,
+                            $arrayName7,$arrayName8,
+                            $arrayName9,$arrayName10,
+                            $arrayName11,$arrayName12
+                        );
+
 
 //$myFile="data1.json";
 //display the array
@@ -56,7 +134,7 @@ while ($row=mysqli_fetch_assoc($result)) {
 //print_r($arrayName);
 //echo '</pre>';
 //convert array into json
-$jsonData = json_encode($arrayName, JSON_PRETTY_PRINT);
+$jsonData = json_encode($final_array, JSON_PRETTY_PRINT);
 //write json into a file
 //if(!file_put_contents($myFile, $jsonData)){
     //echo "no data recorded!";
